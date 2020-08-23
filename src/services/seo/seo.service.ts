dc62a6587914321884dc34f67c3b2cdbc5015c6d
Ivan Grigorov <ivangrigorov9@gmail.com>
2020-08-24 02:27:23 +0300

    some fb sharing and comments moderations documented

--- src/services/seo/seo.service.ts
+++ src/services/seo/seo.service.ts
@@ -24,9 +24,9 @@ export class SEOService {
   }
 
   private updateFBSEO(title: string, description: string, url: string) {
-    this.meta.updateTag({ name: 'og:url', content: url });
-    this.meta.updateTag({ name: 'og:title', content: title })
-    this.meta.updateTag({ name: 'og:description', content: description })
+    this.meta.updateTag({ property: 'og:url', content: url });
+    this.meta.updateTag({ property: 'og:title', content: title })
+    this.meta.updateTag({ property: 'og:description', content: description })
   }
 
   updateKeywords(keywords: string) {
@@ -39,9 +39,9 @@ export class SEOService {
   }
 
   resetMetaTags() {
-    this.meta.removeTag("name='og:url'");
-    this.meta.removeTag("name='og:title'");
-    this.meta.removeTag("name='og:description'");
+    this.meta.removeTag("property='og:url'");
+    this.meta.removeTag("property='og:title'");
+    this.meta.removeTag("property='og:description'");
     this.meta.removeTag("name='twitter:url'");
     this.meta.removeTag("name='twitter:title'");
     this.meta.removeTag("name='twitter:description'");
